7c21a858579d80bc5717c62fe5d7d341117b3b1a
unknown <katsuyama-morito@g.ecc.u-tokyo.ac.jp>
2023-06-27 15:57:56 +0900

    added checking user function

--- insta_match_react/src/choose_user.js
+++ insta_match_react/src/choose_user.js
@@ -1,23 +1,47 @@
 import React, { useState } from "react";
 
-const ChooseUser = ({ onSubmit }) => {
-  const [inputValue, setInputValue] = useState("");
+// ユーザーが特定のusernameをデータベースに登録できるようにする
+// 対象のinstagramページの表示と、確認を伴う
 
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
+const ChooseUser = () => {
+    const [username, setUsername] = useState("");
+    const [confirmed, setConfirmed] = useState(false);
+  
+    const handleChange = (event) => {
+      setUsername(event.target.value);
+    };
+  
+    const handleConfirm = (event) => {
+      event.preventDefault();
+      window.open(`https://www.instagram.com/${username}`, '_blank'); // ユーザーが"ユーザーを確認"ボタンを押したときに新しいタブでInstagramのページを開く
+      setConfirmed(true);
+    };
+  
+    const handleYes = () => {
+      window.localStorage.setItem('chosenUsername', username); // ユーザーが"はい"を選んだときにユーザーネームをlocalStorageに保存
+      setConfirmed(false);
+    };
+  
+    const handleNo = () => {
+      setUsername(""); // ユーザーが"いいえ"を選んだときにユーザーネームをリセット
+      setConfirmed(false);
+    };
+  
+    return (
+      <div>
+        <form onSubmit={handleConfirm}>
+          <input type="text" value={username} onChange={handleChange} placeholder="Enter username" />
+          <button type="submit">ユーザーを確認</button>
+        </form>
+        {confirmed && (
+          <div>
+            <p>このユーザーでよいですか？</p>
+            <button onClick={handleYes}>はい</button>
+            <button onClick={handleNo}>いいえ</button>
+          </div>
+        )}
+      </div>
+    );
   };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onSubmit(inputValue);
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={inputValue} onChange={handleChange} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-
-export default ChooseUser;
+  
+  export default ChooseUser;
