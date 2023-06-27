ccd69bae5710b9196f8bbdb1fe491d77cf8086d3
unknown <katsuyama-morito@g.ecc.u-tokyo.ac.jp>
2023-06-27 14:46:25 +0900

    implemented loading user media

--- insta_match_react/src/InstagramAPI.js
+++ insta_match_react/src/InstagramAPI.js
@@ -4,5 +4,5 @@ import axios from 'axios';
 const baseURL = "https://graph.instagram.com/";
 
 export default function getUserData(access_token) {
-  return axios.get(`${baseURL}me?fields=id,username&access_token=${access_token}`);
+  return axios.get(`${baseURL}me?fields=id,username,media&access_token=${access_token}`);
 }
--- insta_match_react/src/UserComponent.js
+++ insta_match_react/src/UserComponent.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { collection, addDoc, setDoc, doc, getDocs, query, where } from "firebase/firestore";
-import db from './firebase';
-import getUserData from './InstagramAPI';
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  addDoc,
+  setDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import db from "./firebase";
+import getUserData from "./InstagramAPI";
+import axios from "axios";
 
 function UserComponent({ access_token }) {
-  const [userData, setUserData] = useState(JSON.parse(window.localStorage.getItem('userData'))); // ユーザーデータをローカルストレージから読み込む
+  const [userData, setUserData] = useState(
+    JSON.parse(window.localStorage.getItem("userData"))
+  ); // ユーザーデータをローカルストレージから読み込む
+
+  const [userMedia, setUserMedia] = useState([]);
 
   useEffect(() => {
     async function fetchAndSaveUserData() {
@@ -20,32 +33,48 @@ function UserComponent({ access_token }) {
           //await addDoc(collection(db, "users"), userData);
 
           // Set the document with the Instagram user's ID
-          await setDoc(doc(db, 'users', userData.id), userData);
+          await setDoc(doc(db, "users", userData.id), userData);
 
-          
           console.log("Document successfully written!");
 
           // ユーザーデータをセット
           setUserData(userData);
           // ユーザーデータをローカルストレージに保存
-          window.localStorage.setItem('userData', JSON.stringify(userData));
+          window.localStorage.setItem("userData", JSON.stringify(userData));
 
+          // Get user media data
+          const mediaResponse = await axios.get(
+            `https://graph.instagram.com/me/media?fields=id,caption&access_token=${access_token}`
+          );
+          let mediaData = mediaResponse.data.data;
+
+          // Get each media object's detailed data
+          mediaData = await Promise.all(
+            mediaData.map(async (media) => {
+              const detailResponse = await axios.get(
+                `https://graph.instagram.com/${media.id}?fields=id,media_type,media_url,thumbnail_url,permalink,caption&access_token=${access_token}`
+              );
+              return detailResponse.data;
+            })
+          );
+
+          setUserMedia(mediaData);
         } catch (error) {
           console.error("Error writing document: ", error);
           // エラーハンドリングを強化
           if (error.response) {
-            console.error('Response data:', error.response.data);
-            console.error('Response status:', error.response.status);
-            console.error('Response headers:', error.response.headers);
+            console.error("Response data:", error.response.data);
+            console.error("Response status:", error.response.status);
+            console.error("Response headers:", error.response.headers);
           } else if (error.request) {
-            console.error('Request:', error.request);
+            console.error("Request:", error.request);
           } else {
-            console.error('Error', error.message);
+            console.error("Error", error.message);
           }
         }
       }
     }
-    
+
     fetchAndSaveUserData();
   }, [access_token]);
 
@@ -56,6 +85,23 @@ function UserComponent({ access_token }) {
         <>
           <p>User ID: {userData.id}</p>
           <p>Username: {userData.username}</p>
+
+          {/* Display user media data if we have it */}
+          {userMedia.length > 0 ? (
+            userMedia.map((media, index) => (
+              <div key={index}>
+                {media.media_type === "IMAGE" && (
+                  <img src={media.media_url} alt={media.caption} />
+                )}
+                {media.media_type === "VIDEO" && (
+                  <video controls src={media.media_url} />
+                )}
+                <p>{media.caption}</p>
+              </div>
+            ))
+          ) : (
+            <p>Loading user media data...</p>
+          )}
         </>
       ) : (
         <p>Loading user data...</p>
